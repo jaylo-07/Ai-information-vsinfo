86ef4f52b827a0fe9866206d742e3951093b310a
jaykalathiya2005 <jaykalathiyainfotech@gmail.com>
2026-02-27 12:28:56 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://ai.vsinfotech.ca"],
   credentials: true
 }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -48,7 +48,7 @@ app.use(function (err, req, res, next) {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
