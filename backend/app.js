c49e9a2e8b95a2e002ffce276cd4955c339d8568
jaykalathiya2005 <jaykalathiyainfotech@gmail.com>
2026-02-27 10:12:17 +0530

    nodemon

--- backend/app.js
+++ backend/app.js
@@ -39,4 +39,9 @@ app.use(function (err, req, res, next) {
   });
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
